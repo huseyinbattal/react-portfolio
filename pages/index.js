c24bb31490a9d229da9c4ad86e658ec1f0bcf90e
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-01-10 09:27:30 +0300

    change project picture

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,12 @@ import {
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
 import Image from "next/image";
-import web1 from "../public/web1.png";
-import web2 from "../public/web2.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/go-clone.gif";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
+import web1 from "../public/movie.png";
+import web2 from "../public/bookit.png";
+import web3 from "../public/magic.png";
+import web4 from "../public/go-clone.png";
+import web5 from "../public/tic-toc.png";
+import web6 from "../public/personal.png";
 import logo from "../public/logobg.svg";
 
 import Link from "next/link";
@@ -106,9 +106,9 @@ export default function Home() {
         </section>
         <hr />
         <section className="py-10">
-          <h1 className=" text-4xl py-1  dark:text-white text-center font-mono text-blue-900 ">
+          <h1 className=" text-xl sm:text-3xl py-1  dark:text-white text-center font-mono text-blue-900 ">
             <div className="flex justify-center space-x-4">
-              <span>My Projects</span> <BsFillArrowDownSquareFill className="hover:text-red-800 text-teal-600 cursor-pointer" onClick={down} />
+              <span>My Projects</span> <BsFillArrowDownSquareFill className="hover:text-red-800 text-teal-600 cursor-pointer dark:hover:text-white dark:text-gray-500 " onClick={down} />
             </div>
           </h1>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap ">
