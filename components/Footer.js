3df77c717f2cad58510ab0ab5f88face99bd2621
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-01-10 10:32:03 +0300

    add footer dark mode

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
     <>
-      <footer className="flex flex-col sm:flex-row  items-center text-center gap-x-10 gap-y-3 justify-center mb-5 font-mono text-blue-800 p-2 text-sm">
+      <footer className="flex flex-col sm:flex-row  items-center text-center gap-x-10 dark:bg-gray-900 dark:text-white gap-y-3 justify-center mb-5 font-mono text-blue-800 p-2 text-sm">
         <span>© {new Date().getFullYear()} / Hüseyin BATTAL</span>
         <p className="mb-0">
           <a
-            className="e-mail"
+            className="e-mail hover:text-red-500 hover:underline hover:dark:text-teal-400"
             href="https://www.huseyinbattal.online/"
             rel="noopener noreferrer"
           >
